b10aa0ea26cb6568a54a6d3c99db4554a4eda862
DwarfPorter <mrvlmor@gmail.com>
2019-07-22 21:02:08 +0300

    Отображение RecyclerView

--- app/src/main/java/ru/geekbrains/socialnetwork/MainActivity.java
+++ app/src/main/java/ru/geekbrains/socialnetwork/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.geekbrains.socialnetwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] data = getResources().getStringArray(R.array.items);
+        initRecyclerView(data);
+    }
+
+    private void initRecyclerView(String[] data)
+    {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        SocialNetworkAdapter socnetAdapter = new SocialNetworkAdapter(data);
+        recyclerView.setAdapter(socnetAdapter);
     }
 }
